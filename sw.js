508c4f43ca8b2fae5f43dda8b2377f0be3892339
Ahmed-fazari81 <ahmedmuk34@gmail.com>
2026-02-18 14:28:11 +0400

    Update service worker cache name and files

--- sw.js
+++ sw.js
@@ -1,18 +1,17 @@
-const CACHE = "cybershield-v2";
-
+const CACHE_NAME = "cybershield-v3";
 self.addEventListener("install", e => {
     e.waitUntil(
-        caches.open(CACHE).then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             return cache.addAll([
-                "/",
-                "/index.html",
-                "/manifest.json",
-                "/icon-192.png"
+                "./",
+                "./index.html",
+                "./app.js",
+                "./manifest.json",
+                "./icon-192.png"
             ]);
         })
     );
 });
-
 self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(res => res || fetch(e.request))
